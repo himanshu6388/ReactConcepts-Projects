f71b7e7edb09f4eef57123649f50fb32e52082cf
himanshu6388 <himanshu219181@gmail.com>
2025-01-22 08:01:29 +0530

    updated

--- gsap/script.js
+++ gsap/script.js
@@ -1,5 +1,27 @@
-gsap.to("#box", {
-    x:1000,
-    duration:2,
-    delay:1
+const t1 = gsap.timeline();
+t1.to("#box",{
+    x:0,
+    backgroundColor:"green",
+    duration:1,
+    repeat:-1
+})
+// .to(".box2", {
+//     duration:1,
+//     x:400,
+//     y:200
+// })
+// .to(".box3", {
+//     duration:2,
+//     y:-200,
+//     x:400,
+//     rotation:360
+// })
+
+t1.from("h1",{
+    opacity:0,
+    y:20,
+    duration:1,
+    stagger:0.6,
+    delay:0,
+    repeat:1
 })
